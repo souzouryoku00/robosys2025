3860710a1100b6618b1d9b2947803db0a4f92a74
souzou <s24c1109hv@s.chibakoudai.jp>
2025-10-30 13:07:09 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
 	echo ${1}行目が違うよ
@@ -9,6 +9,15 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15.0 ] || ng "$LINENO"
++ '[' 15 = 15 ']'
 
-[ "${res}" = 0 ] && echo OK
+out=$(seq あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(seq | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
