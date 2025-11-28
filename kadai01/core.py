5cbba007517bc062e85fa7cad14a4be12afe30aa
souzou <s24c1109hv@s.chibakoudai.jp>
2025-11-28 13:04:16 +0900

    made

--- kadai01/core.py
+++ kadai01/core.py
@@ -3,3 +3,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import re
+from collections import Counter
+
+def analyze_text(text: str) -> dict:
+    lines = text.splitlines()
+     words = re.findall(r'\S+', text)
+     alphabets = sum(c.isalpha() for c in text)
